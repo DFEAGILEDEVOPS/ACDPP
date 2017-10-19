41e4dd7e95e36862cff1b25c54ecf188475a23fc
Stephen McCabe <stephen.mccabe@cadencepartnership.com>
2017-10-19 15:28:33 +0100

    Refactored to use common CallJsonApiAsync

--- Dashboard/Classes/VSTSManager.cs
+++ Dashboard/Classes/VSTSManager.cs
@@ -56,7 +56,7 @@ namespace Dashboard.Classes
 
         public static List<Project> GetProjects()
         {
-            var json = Task.Run(async () => await GetProjectsAsync()).Result;
+            var json = Task.Run(async () => await Web.CallJsonApiAsync(HttpMethod.Get, $"https://{Account}.visualstudio.com/DefaultCollection/_apis/projects/?api-version={ApiVersion}&stateFilter=All", password: AppSettings.VSTSPersonalAccessToken)).Result;
             var results = new List<Project>();
             foreach (var item in json.value)
             {
@@ -72,93 +72,38 @@ namespace Dashboard.Classes
             return results;
         }
 
-        public static async Task<dynamic> GetProjectsAsync()
+        public static string CreateProject(string name, string description=null)
         {
-            using (HttpClient client = new HttpClient())
+            var body = new
             {
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", AppSettings.VSTSPersonalAccessToken))));
-
-                using (HttpResponseMessage response = client.GetAsync($"https://{Account}.visualstudio.com/DefaultCollection/_apis/projects/?api-version={ApiVersion}&stateFilter=All").Result)
+                name,
+                description,
+                capabilities = new
                 {
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
-                    return jsonResponse;
-                }
-            }
-        }
-
-        public static string CreateProject(string projectName)
-        {
-            var json = Task.Run(async () => await CreateProjectAsync(projectName)).Result;
-            return json.id;
-        }
-
-        public static async Task<dynamic> CreateProjectAsync(string name, string description=null)
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", AppSettings.VSTSPersonalAccessToken))));
-
-                var body = new
-                {
-                    name,
-                    description,
-                    capabilities=new
+                    versioncontrol = new
                     {
-                        versioncontrol = new
-                        {
-                            sourceControlType="Git"
-                        },
-                        processTemplate = new
-                        {
-                            templateTypeId=ProjectTemplateId
-                        }
+                        sourceControlType = "Git"
+                    },
+                    processTemplate = new
+                    {
+                        templateTypeId = ProjectTemplateId
                     }
-                };
-
-                var json = JsonConvert.SerializeObject(body);
-                var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
-
-                using (HttpResponseMessage response = client.PostAsync($"https://{Account}.visualstudio.com/DefaultCollection/_apis/projects/?api-version={ApiVersion}", httpContent).Result)
-                {
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
-                    return jsonResponse;
                 }
-            }
+            };
+
+            var json = Task.Run(async () => await Web.CallJsonApiAsync(HttpMethod.Post, $"https://{Account}.visualstudio.com/DefaultCollection/_apis/projects/?api-version={ApiVersion}", password: AppSettings.VSTSPersonalAccessToken,body:body)).Result;
+            return json.id;
         }
 
         public static string DeleteProject(string projectId)
         {
-            var json = Task.Run(async () => await DeleteProjectAsync(projectId)).Result;
+            var json = Task.Run(async () => await Web.CallJsonApiAsync(HttpMethod.Delete, $"https://{Account}.visualstudio.com/DefaultCollection/_apis/projects/{projectId}?api-version={ApiVersion}", password: AppSettings.VSTSPersonalAccessToken)).Result;
             return json.id;
         }
 
-        public static async Task<dynamic> DeleteProjectAsync(string projectId)
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", AppSettings.VSTSPersonalAccessToken))));
-
-                using (HttpResponseMessage response = client.DeleteAsync($"https://{Account}.visualstudio.com/DefaultCollection/_apis/projects/{projectId}?api-version={ApiVersion}").Result)
-                {
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
-                    return jsonResponse;
-                }
-            }
-        }
-
-
         public static List<Team> GetTeams(string projectId)
         {
-            var json = Task.Run(async () => await GetTeamsAsync(projectId)).Result;
+            var json = Task.Run(async () => await Web.CallJsonApiAsync(HttpMethod.Get, $"https://{Account}.visualstudio.com/DefaultCollection/_apis/projects/{projectId}/teams?api-version={ApiVersion}", password: AppSettings.VSTSPersonalAccessToken)).Result;
             var results = new List<Team>();
             foreach (var item in json.value)
             {
@@ -174,26 +119,9 @@ namespace Dashboard.Classes
             return results;
         }
 
-        public static async Task<dynamic> GetTeamsAsync(string projectId)
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", AppSettings.VSTSPersonalAccessToken))));
-
-                using (HttpResponseMessage response = client.GetAsync($"https://{Account}.visualstudio.com/DefaultCollection/_apis/projects/{projectId}/teams?api-version={ApiVersion}").Result)
-                {
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
-                    return jsonResponse;
-                }
-            }
-        }
-
         public static List<Member> GetMembers(string projectId, string teamId)
         {
-            var json = Task.Run(async () => await GetMembersAsync(projectId, teamId)).Result;
+            var json = Task.Run(async () => await Web.CallJsonApiAsync(HttpMethod.Get, $"https://{Account}.visualstudio.com/DefaultCollection/_apis/projects/{projectId}/teams/{teamId}/members?api-version={ApiVersion}", password: AppSettings.VSTSPersonalAccessToken)).Result;
             var results = new List<Member>();
             foreach (var item in json.value)
             {
@@ -209,26 +137,9 @@ namespace Dashboard.Classes
             return results;
         }
 
-        public static async Task<dynamic> GetMembersAsync(string projectId, string teamId)
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", AppSettings.VSTSPersonalAccessToken))));
-
-                using (HttpResponseMessage response = client.GetAsync($"https://{Account}.visualstudio.com/DefaultCollection/_apis/projects/{projectId}/teams/{teamId}/members?api-version={ApiVersion}").Result)
-                {
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
-                    return jsonResponse;
-                }
-            }
-        }
-
         public static List<Repo> GetRepos(string projectId)
         {
-            var json = Task.Run(async () => await GetReposAsync(projectId)).Result;
+            var json = Task.Run(async () => await Web.CallJsonApiAsync(HttpMethod.Get, $"https://{Account}.visualstudio.com/DefaultCollection/{projectId}/_apis/git/repositories?api-version={ApiVersion}", password: AppSettings.VSTSPersonalAccessToken)).Result;
             var results = new List<Repo>();
             foreach (var item in json.value)
             {
@@ -252,125 +163,50 @@ namespace Dashboard.Classes
             return results;
         }
 
-        public static async Task<dynamic> GetReposAsync(string projectId)
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", AppSettings.VSTSPersonalAccessToken))));
-
-                using (HttpResponseMessage response = client.GetAsync($"https://{Account}.visualstudio.com/DefaultCollection/{projectId}/_apis/git/repositories?api-version={ApiVersion}").Result)
-                {
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
-                    return jsonResponse;
-                }
-            }
-        }
-
         public static string CreateEndpoint(string projectId, string sourceUrl, string sourceUsername, string sourcePassword)
         {
-            var json = Task.Run(async () => await CreateEndpointAsync(projectId, sourceUrl, sourceUsername, sourcePassword)).Result;
+            const string apiVersion = "3.0-preview.1";
+
+            var body = new
+            {
+                name = "HelloWorld-Git-" + Guid.NewGuid(),
+                type = "Git",
+                url = sourceUrl,
+
+                authorization = new
+                {
+                    scheme = "UsernamePassword",
+                    parameters = new
+                    {
+                        username = sourceUsername,
+                        password = sourcePassword
+                    },
+                }
+            };
+
+            var json = Task.Run(async () => await Web.CallJsonApiAsync(HttpMethod.Post, $"https://{Account}.visualstudio.com/DefaultCollection/{projectId}/_apis/distributedtask/serviceendpoints?api-version={apiVersion}", password: AppSettings.VSTSPersonalAccessToken, body: body)).Result;
             return json.id;
         }
 
-        public static async Task<dynamic> CreateEndpointAsync(string projectId, string sourceUrl, string sourceUsername, string sourcePassword)
-        {
-            const string apiVersion = "3.0-preview.1";
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", AppSettings.VSTSPersonalAccessToken))));
-
-                var body = new
-                {
-                    name="HelloWorld-Git-"+Guid.NewGuid(),
-                    type="Git",
-                    url = sourceUrl,
-
-                    authorization = new
-                    {
-                        scheme="UsernamePassword",
-                        parameters = new
-                        {
-                            username=sourceUsername,
-                            password=sourcePassword
-                        },
-                    }
-                };
-
-                var json = JsonConvert.SerializeObject(body);
-                var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
-
-                using (HttpResponseMessage response = client.PostAsync($"https://{Account}.visualstudio.com/DefaultCollection/{projectId}/_apis/distributedtask/serviceendpoints?api-version={apiVersion}", httpContent).Result)
-                {
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
-                    return jsonResponse;
-                }
-            }
-        }
         public static string ImportRepo(string projectId, string repoId, string sourceUrl, string serviceEndpointId)
         {
-            var json = Task.Run(async () => await ImportRepoAsync(projectId, repoId, sourceUrl,serviceEndpointId)).Result;
+            const string apiVersion = "3.0-preview";
+
+            var body = new
+            {
+                parameters = new
+                {
+                    gitSource = new
+                    {
+                        url = sourceUrl
+                    },
+                    serviceEndpointId,
+                    deleteServiceEndpointAfterImportIsDone = true
+                }
+            };
+            var json = Task.Run(async () => await Web.CallJsonApiAsync(HttpMethod.Post, $"https://{Account}.visualstudio.com/DefaultCollection/{projectId}/_apis/git/repositories/{repoId}/importRequests?api-version={apiVersion}", password: AppSettings.VSTSPersonalAccessToken, body: body)).Result;
             return json.importRequestId;
         }
 
-        public static async Task<dynamic> ImportRepoAsync(string projectId,string repoId, string sourceUrl,string serviceEndpointId)
-        {
-            const string apiVersion = "3.0-preview";
-            using (HttpClient client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", AppSettings.VSTSPersonalAccessToken))));
-
-                var body = new
-                {
-                    parameters = new
-                    {
-                        gitSource = new
-                        {
-                            url = sourceUrl
-                        },
-                        serviceEndpointId,
-                        deleteServiceEndpointAfterImportIsDone=true
-                    }
-                };
-
-                var json = JsonConvert.SerializeObject(body);
-                var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
-
-                using (HttpResponseMessage response = client.PostAsync($"https://{Account}.visualstudio.com/DefaultCollection/{projectId}/_apis/git/repositories/{repoId}/importRequests?api-version={apiVersion}", httpContent).Result)
-                {
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
-                    return jsonResponse;
-                }
-            }
-        }
-        //public static void CloneRepo(string sourceUrl, string sourceUsername, string sourcePassword, string targetUrl=null, string targetUsername=null, string targetPassword=null)
-        //{
-        //    if (string.IsNullOrWhiteSpace(targetUrl)) targetUrl = sourceUrl;
-        //    if (string.IsNullOrWhiteSpace(targetUsername)) targetUsername = sourceUsername;
-        //    if (string.IsNullOrWhiteSpace(targetPassword)) targetPassword = sourcePassword;
-
-        //     var cloneOptions = new CloneOptions() { CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = sourceUsername, Password = sourcePassword } };
-        //    var pushOptions = new PushOptions() { CredentialsProvider = new CredentialsHandler((url, usernameFromUrl, types) => new UsernamePasswordCredentials() { Username = targetUsername, Password = targetPassword }) };
-
-
-        //    using (var tmpDir = new TemporaryDirectory())
-        //    {
-        //        var path=Repository.Clone(sourceUrl, tmpDir.FullName, cloneOptions);
-
-        //        using (var sourceRepo = new Repository(path))
-        //        {
-        //            var remote=sourceRepo.Network.Remotes.Add("origin1", targetUrl);
-        //            sourceRepo.Network.Push(remote, @"refs/heads/master", pushOptions);
-        //        }
-        //    }
-        //}
     }
 }
--- Extensions/Web.cs
+++ Extensions/Web.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace Extensions
 {
@@ -27,5 +29,44 @@ namespace Extensions
                 return await client.DownloadDataTaskAsync(uri);
             }
         }
+
+        public static async Task<dynamic> CallJsonApiAsync(HttpMethod httpMethod, string url, string username=null, string password=null, object body=null)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                if (!string.IsNullOrWhiteSpace(username) || !string.IsNullOrWhiteSpace(password))
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}")));
+                }
+
+                HttpContent httpContent=null;
+                if (body != null)
+                {
+                    if (!httpMethod.IsAny(HttpMethod.Post, HttpMethod.Put))throw new ArgumentOutOfRangeException(nameof(httpMethod), "HttpMethod must be Post or Put when a body is specified");
+
+                    var json = JsonConvert.SerializeObject(body);
+                    httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+                }
+                else if (httpMethod.IsAny(HttpMethod.Post, HttpMethod.Put))
+                    throw new ArgumentOutOfRangeException(nameof(body), "You must supply a body when Post or Put when a body is specified");
+
+
+                using (var response = (
+                    httpMethod == HttpMethod.Get ? client.GetAsync(url) :
+                    httpMethod == HttpMethod.Delete ? client.DeleteAsync(url) :
+                    httpMethod == HttpMethod.Post ? client.PostAsync(url, httpContent) :
+                    httpMethod == HttpMethod.Put ? client.PutAsync(url, httpContent):
+                    throw new ArgumentOutOfRangeException(nameof(httpMethod),"HttpMethod must be Get, Delete, Post or Put")
+                    ).Result)
+                {
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseBody);
+                    return jsonResponse;
+                }
+            }
+        }
+
     }
 }
