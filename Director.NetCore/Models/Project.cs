1d0f9e8f69a3d18b7d53a0ec4eb38f11bc3fd779
Stephen McCabe <stephen.mccabe@cadencepartnership.com>
2017-12-20 16:24:30 +0000

    Started coding director

---
+++ Director.NetCore/Models/Project.cs
@@ -0,0 +1,252 @@
+﻿using Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Director.NetCore.Models
+{
+    public class Project
+    {
+        public class Team
+        {
+            public class Member
+            {
+                [XmlAttribute]
+                public string Email { get; set; }
+                [XmlAttribute]
+                public string Role { get; set; }
+
+                internal void Validate(Project project,List<Exception> exceptions)
+                {
+                    if (string.IsNullOrWhiteSpace(Email))
+                        exceptions.Add(new ArgumentNullException(nameof(Email), $"Team {nameof(Email)} cannot be empty"));
+                    else if (!Email.IsEmailAddress())
+                        exceptions.Add(new ArgumentNullException(nameof(Email), $"Team member {nameof(Email)} '{Email}' is not a valid email address"));
+
+                    if (string.IsNullOrWhiteSpace(Role))
+                        exceptions.Add(new ArgumentNullException(nameof(Role), $"Team {nameof(Role)} cannot be empty"));
+
+                }
+            }
+
+            [XmlAttribute]
+            public string Name { get; set; }
+            public List<Member> Members { get; set; }
+
+            internal void Validate(Project project,List<Exception> exceptions)
+            {
+                if (string.IsNullOrWhiteSpace(Name)) exceptions.Add(new ArgumentNullException(nameof(Name), $"Team {nameof(Name)} cannot be empty"));
+
+                if (Members == null || Members.Count == 0) exceptions.Add(new ArgumentNullException(nameof(Members), $"{nameof(Members)} you must have at least one team member"));
+                foreach (var member in Members) member.Validate(project,exceptions);
+            }
+        }
+
+        public class Repository
+        {
+            [XmlAttribute]
+            public string Name { get; set; }
+
+            internal void Validate(Project project,List<Exception> exceptions)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class AppRegistration
+        {
+            [XmlAttribute]
+            public string Name { get; set; }
+            [XmlAttribute]
+            public string Url { get; set; }
+
+            internal void Validate(Project project,List<Exception> exceptions)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        public class ResourceGroup
+        {
+            public class KeyVault
+            {
+                [XmlAttribute]
+                public string Name { get; set; }
+
+                internal void Validate(Project project,List<Exception> exceptions)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+
+            public class SqlServer
+            {
+                public class Database
+                {
+                    [XmlAttribute] public string Name { get; set; }
+                }
+
+                public class FirewallRule
+                {
+                    [XmlAttribute]
+                    public string Name { get; set; }
+                    [XmlAttribute]
+                    public string StartIP { get; set; }
+                    [XmlAttribute]
+                    public string EndIP { get; set; }
+                }
+
+                [XmlAttribute]
+                public string Name { get; set; }
+                [XmlAttribute]
+                public string PricingTier { get; set; }
+                [XmlAttribute]
+                public string KeyVaultName { get; set; }
+                [XmlAttribute]
+                public string UsernameSecretName { get; set; }
+                [XmlAttribute]
+                public string PasswordSecretName { get; set; }
+                public List<Database> Databases { get; set; }
+                public List<FirewallRule> FirewallRules { get; set; }
+                [XmlAttribute]
+                public bool AllowAzureAccess { get; set; }
+
+                internal void Validate(Project project,List<Exception> exceptions)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public class StorageAccount
+            {
+                public class BlobContainer
+                {
+                    [XmlAttribute]
+                    public string Name { get; set; }
+                }
+
+                public class FilesShare
+                {
+                    [XmlAttribute]
+                    public string Name { get; set; }
+                }
+
+                public class Table
+                {
+                    [XmlAttribute]
+                    public string Name { get; set; }
+                }
+
+                public class Queue
+                {
+                    [XmlAttribute]
+                    public string Name { get; set; }
+                }
+                [XmlAttribute]
+                public string Name { get; set; }
+                [XmlAttribute]
+                public string KeyVaultName { get; set; }
+                [XmlAttribute]
+                public string AccessKeySecretName { get; set; }
+                public List<BlobContainer> BlobContainers { get; set; }
+                public List<FilesShare> FilesShares { get; set; }
+                public List<Table> Tables { get; set; }
+                public List<Queue> Queues { get; set; }
+
+                internal void Validate(Project project,List<Exception> exceptions)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+
+            public class RedisCache
+            {
+                [XmlAttribute]
+                public string Name { get; set; }
+                [XmlAttribute]
+                public string PricingTier { get; set; }
+                [XmlAttribute]
+                public string KeyVaultName { get; set; }
+                [XmlAttribute]
+                public string AccessKeySecretName { get; set; }
+
+                internal void Validate(Project project,List<Exception> exceptions)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+            [XmlAttribute]
+            public string Name { get; set; }
+            [XmlAttribute]
+            public string Region { get; set; }
+            public List<KeyVault> KeyVaults { get; set; }
+            public List<SqlServer> SqlServers { get; set; }
+            public List<StorageAccount> StorageAccounts { get; set; }
+            public List<RedisCache> RedisCaches { get; set; }
+
+            internal void Validate(Project project,List<Exception> exceptions)
+            {
+                if (string.IsNullOrWhiteSpace(Name)) exceptions.Add(new ArgumentNullException(nameof(Name), $"Resource Group {nameof(Name)} cannot be empty"));
+                if (string.IsNullOrWhiteSpace(Region)) exceptions.Add(new ArgumentNullException(nameof(Region), $"Resource Group {nameof(Region)} cannot be empty"));
+
+                if (KeyVaults == null || KeyVaults.Count == 0) exceptions.Add(new ArgumentNullException(nameof(KeyVaults), $"{nameof(KeyVaults)} you must have at least one Key Vault"));
+                foreach (var vault in KeyVaults) vault.Validate(project,exceptions);
+                foreach (var server in SqlServers) server.Validate(project,exceptions);
+                foreach (var storage in StorageAccounts) storage.Validate(project,exceptions);
+                foreach (var cache in RedisCaches) cache.Validate(project,exceptions);
+            }
+        }
+
+        internal void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class Deletion
+        {
+            [XmlAttribute]
+            public string Type { get; set; }
+            [XmlAttribute]
+            public string Name { get; set; }
+
+            internal void Validate(Project project, List<Exception> exceptions)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        internal void Validate()
+        {
+            var exceptions = new List<Exception>();
+
+            if (string.IsNullOrWhiteSpace(ProjectName)) exceptions.Add(new ArgumentNullException(nameof(ProjectName), $"{nameof(ProjectName)} cannot be empty"));
+
+            if (Teams == null || Teams.Count == 0) exceptions.Add(new ArgumentNullException(nameof(Teams), $"{nameof(Teams)} you must have at least one team"));
+            foreach (var team in Teams) team.Validate(this,exceptions);
+
+            if (ResourceGroups==null || Teams.Count==0) exceptions.Add(new ArgumentNullException(nameof(ResourceGroups), $"{nameof(ResourceGroups)} you must have at least one resource group"));
+            foreach (var group in ResourceGroups) group.Validate(this,exceptions);
+
+            if (Repositories==null || Repositories.Count==0) exceptions.Add(new ArgumentNullException(nameof(Repositories), $"{nameof(Repositories)} you must have at least one repository"));
+            foreach (var repo in Repositories) repo.Validate(this,exceptions);
+
+            if (AppRegistrations == null || AppRegistrations.Count==0) exceptions.Add(new ArgumentNullException(nameof(AppRegistrations), $"{nameof(AppRegistrations)} you must have at least one App Registration"));
+            foreach (var app in AppRegistrations) app.Validate(this,exceptions);
+
+            foreach (var deletion in Deletions) deletion.Validate(this,exceptions);
+
+            if (exceptions.Count > 0) throw new AggregateException(exceptions);
+        }
+
+        public string ProjectName { get; set; }
+        public List<Team> Teams { get; set; }
+        public List<Repository> Repositories { get; set; }
+        public List<ResourceGroup> ResourceGroups { get; set; }
+        public List<AppRegistration> AppRegistrations { get; set; }
+        public List<Deletion> Deletions { get; set; }
+    }
+}
---
+++ Director.NetCore/Program.cs
@@ -0,0 +1,170 @@
+﻿using Director.NetCore.Models;
+using Extensions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Director.NetCore
+{
+    class Program
+    {
+        private static Dictionary<string, string> Parameters = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        private static List<string> Scripts = new List<string>();
+        private static string ProjectId;
+        private string ProjectKey;
+        private const string HelpMessage = "Usage:\n\nDirector.exe [-s|--script] <scriptfilepath> [-p|--project] <projectId> [-k|--key] <securitykey>\n" 
+            + "e.g., director \"c:\\myscript.json\" 360A4F23-7495-46EA-849E-6328A77546C6 ADD90C25-CD35-4E88-AC93-2248A1D9A0F3\n\n";
+
+        enum ExitCode : int
+        {
+            Success = 0,
+            BadArguments=1,
+            BadScript = 2,
+            BadProject = 3,
+            BadSecurityKey = 4,
+            UnknownError = 10
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                #region Get the parameters
+                //Show help if no arguments
+                if (args == null || args.Length != 3) ShowHelp();
+
+                for (int i = 0; i < args.Length; i++)
+                {
+                    var arg = args[i];
+                    if (string.IsNullOrWhiteSpace(arg)) continue;
+                    var name = arg.BeforeFirstAny(@"-: /\").TrimI(Text.PunctuationChars + " ");
+                    var value = arg.AfterLastAny(@"-: /\").TrimI(Text.PunctuationChars + " ");
+                    if (!string.IsNullOrWhiteSpace(name) && !string.IsNullOrWhiteSpace(value))
+                    {
+                        if (name.EqualsI("s", "script"))
+                            Parameters["script"] = value;
+                        else if (name.EqualsI("p", "project", "projectid"))
+                            Parameters["project"] = value;
+                        else if (name.EqualsI("k", "key", "securitykey"))
+                            Parameters["key"] = value;
+                        continue;
+                    }
+                    arg = arg.TrimI(Text.PunctuationChars + " ");
+                    if (string.IsNullOrWhiteSpace(arg)) continue;
+
+                    if (i == 0 && arg.EndsWithI(".json", ".xml"))
+                    {
+                        Parameters["script"] = arg;
+                        continue;
+                    }
+                    if (i == 1)
+                    {
+                        Parameters["project"] = arg;
+                        continue;
+                    }
+                    if (i == 2)
+                    {
+                        Parameters["key"] = arg;
+                        continue;
+                    }
+                }
+                if (Parameters.Count != 3) ShowHelp();
+                #endregion
+
+                #region Check the parameters
+                //Make sure the file exists
+                if (!File.Exists(Parameters["Script"]))
+                {
+                    Console.WriteLine($"Cannot find script file '{Parameters["Script"]}'");
+                    Environment.Exit((int)ExitCode.BadScript);
+                }
+
+                //Make sure we can read the json contents
+                string json = null;
+                try
+                {
+                    json = File.ReadAllText(Parameters["Script"]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error reading script file '{Parameters["Script"]}': {ex.Message}");
+                    Environment.Exit((int)ExitCode.BadScript);
+                }
+
+                if (string.IsNullOrWhiteSpace(json))
+                {
+                    Console.WriteLine($"Script file '{Parameters["Script"]}' cannot be empty");
+                    Environment.Exit((int)ExitCode.BadScript);
+                }
+
+                //Make sure we can deserialize the json
+                Project project = null;
+                try
+                {
+                    var ext = Path.GetExtension(Parameters["Script"];
+                    if (ext.EqualsI(".json"))
+                        project = JsonConvert.DeserializeObject<Project>(json);
+                    else
+                        throw new NotImplementedException($"Deserialization of file type '{ext}' has not yet been implemented");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error deserializing script file '{Parameters["Script"]}': {ex.Message}");
+                    Environment.Exit((int)ExitCode.BadScript);
+                }
+
+                //Make sure the project is valid
+                try
+                {
+                    project.Validate();
+                }
+                catch (AggregateException aex)
+                {
+                    Console.WriteLine($"Script '{Parameters["Script"]}' has the following validation errors:");
+                    foreach (var ex in aex.InnerExceptions)
+                        Console.WriteLine(ex.Message);
+
+                    Environment.Exit((int)ExitCode.BadScript);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Script '{Parameters["Script"]}' has the following validation error:\n{ex.Message}");
+                    Environment.Exit((int)ExitCode.BadScript);
+                }
+
+                //TODO Check the credentials
+                Environment.Exit((int)ExitCode.BadProject);
+                Environment.Exit((int)ExitCode.BadSecurityKey);
+
+                #endregion
+
+
+                //Execute the project
+                project.Execute();
+
+                //Exit with success
+                Console.WriteLine($"Script '{Parameters["Script"]}' completed successfully");
+                Environment.Exit((int)ExitCode.Success);
+            }
+            catch (AggregateException aex)
+            {
+                Console.WriteLine($"Script '{Parameters["Script"]}' failed with following errors:");
+                foreach (var ex in aex.InnerExceptions)
+                    Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Script '{Parameters["Script"]}' failed with following error:\n{ex.Message}");
+            }
+
+            Environment.Exit((int)ExitCode.UnknownError);
+        }
+
+        static void ShowHelp()
+        {
+            Console.WriteLine(HelpMessage);
+            Environment.Exit((int)ExitCode.BadArguments);
+        }
+    }
+}
